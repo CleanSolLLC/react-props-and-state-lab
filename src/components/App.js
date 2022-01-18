f05667fbc550d04fc473c0edc1b65b844b7fc068
mamark90 <maqmark90@gmail.com>
2022-01-18 13:56:25 -0500

    commit tree 8867992e7e1530879ab501a5149b00ffaa193b36

--- src/components/App.js
+++ src/components/App.js
@@ -10,10 +10,45 @@ class App extends React.Component {
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
+        type: 'all',
       }
     }
-  }
+  };
+
+  changeType = (event)=> {
+   this.setState({
+     filters: {
+       type: event.target.value,
+     }
+   });
+  };
+
+  setPets = (listOfPets) => {
+    this.setState({
+        pets: listOfPets,
+      
+    });
+  };
+
+  findPets = () => {
+    let url = "/api/pets" 
+    if (this.state.filters.type === "all") {
+      url = "/api/pets"
+    } else {
+      url = url + "?type=" + this.state.filters.type
+    };
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setPets(data)); 
+  };
+
+  adoptPet = (id) => {
+    let pet = this.state.pets.find(pet => pet.id === id)
+    pet.isAdopted = true
+  };
+
+
 
   render() {
     return (
@@ -24,10 +59,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.changeType} onFindPetsClick={this.findPets}  />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.adoptPet} pets={this.state.pets} />
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,11 +1,17 @@
 import React from 'react'
-
-import Pet from './Pet'
+import Pet from './Pet';
 
 class PetBrowser extends React.Component {
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
-  }
-}
+    return (
+      this.props.pets.map((petDetails, idx) => {
+        return <div className="ui cards">
+          <Pet pet={petDetails} key={idx} onAdoptPet={this.props.onAdoptPet}/>
+        </div>
+      })
+
+    )
+  };
+};
 
 export default PetBrowser
